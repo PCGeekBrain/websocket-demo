593fd2720d87cb8001409d5d3de6d2ecc0c207c3
Menachem Hornbacher <mendelh1537@gmail.com>
2018-05-14 18:27:25 -0400

    simple node server to send index.html to client

---
+++ client/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get( '*', ( req, res ) => {
+    console.log( `${req.method} ${req.originalUrl} - ${req.ip}` );
+    res.sendFile( path.join( __dirname + "/index.html" ) );
+});
+
+app.listen( PORT, () => {
+    console.log( `Serving index.html on http://localhost:${PORT}` );
+})
